7edd0cdbfad1b1b280b20c64a54d58d87d4b3334
dimryb@bk.ru <dimryb@bk.ru>
2023-10-11 23:05:30 +0700

    Добавил тесты в проект

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,25 @@ subprojects {
         kotlinOptions.jvmTarget = "17"
     }
 }
+
+dependencies {
+    val kotestVersion: String by project
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+}
+
+tasks {
+    withType<Test>().configureEach {
+        useJUnitPlatform()
+    }
+    test {
+        systemProperty("kotest.framework.test.severity", "NORMAL")
+    }
+    create<Test>("test-strict") {
+        systemProperty("kotest.framework.test.severity", "MINOR")
+        group = "verification"
+    }
+}
