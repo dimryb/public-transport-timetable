d6969c04a4f7a8862749561fe9256e19df69966c
dimryb@bk.ru <dimryb@bk.ru>
2023-10-12 23:37:45 +0700

    Изменил архитектуру проекта

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,17 +15,8 @@ subprojects {
     version = "1.0-SNAPSHOT"
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "19"
     }
-}
-
-dependencies {
-    val kotestVersion: String by project
-
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
-    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 tasks {
