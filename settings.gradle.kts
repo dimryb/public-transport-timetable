3b47371aeb4d227421e434f90a8fb32eb0cadf17
dimryb <dimryb@bk.ru>
2023-10-31 23:38:47 +0700

    Добавил build gradle для ktor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "public-transport-timetable"
 pluginManagement {
     val kotlinVersion: String by settings
     val openapiVersion: String by settings
+    val ktorVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion apply false
@@ -10,6 +11,8 @@ pluginManagement {
         kotlin("plugin.serialization") version kotlinVersion apply false
 
         id("org.openapi.generator") version openapiVersion apply false
+
+        id("io.ktor.plugin") version ktorVersion apply false
     }
 }
 
@@ -18,3 +21,4 @@ include("public-transport-timetable-acceptance")
 include("public-transport-timetable-api-v1-jackson")
 include("public-transport-timetable-common")
 include("public-transport-timetable-mappers-v1")
+include("public-transport-timetable-app-ktor")
